9a93061b2fd131b29f715a8e33752f34c2828c81
Chris Riebschlager <riebschlager@gmail.com>
2013-04-20 12:22:37 -0500

    css updates

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,8 @@
 $('audio').removeAttr('controls').each(function(i, audioElement) {
 	var audio = $(this);
 	var that = this;
-	$('.hero-unit').append($('<button class="btn btn-inverse">' + audio.attr('title') + '</button>').click(function() {
+	$('.hero-unit').append($('<button class="btn btn-large btn-inverse">' + audio.attr('title') + 
+'</button>').click(function() {
 		that.play();
 	}))
 });
