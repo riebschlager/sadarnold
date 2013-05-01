a6ed321b27a72ded89286a31968f7ab8a0da9db8
Chris Riebschlager <riebschlager@gmail.com>
2013-05-01 16:35:54 -0500

    mobile fixes

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,7 @@
 $('audio').removeAttr('controls').each(function(i, audioElement) {
-	var audio = $(this);
-	var that = this;
-	$('.hero-unit').append($('<button class="btn btn-large btn-inverse">' + audio.attr('title') + 
-'</button>').click(function() {
-		that.play();
-	}))
+    var audio = $(this);
+    var that = this;
+    $('#board').append($('<button class="btn btn-large btn-inverse">' + audio.attr('title') + '</button>').click(function() {
+        that.play();
+    }))
 });
